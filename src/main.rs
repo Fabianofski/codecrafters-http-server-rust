e993d047860599c69f50f883bf8fff2c494456e3
Fabian Friedrich <friedrich.fabian@gmx.net>
2024-07-12 21:13:39 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 use std::{io::Write, io::Read, collections::HashMap, net::TcpListener};
 
 fn extract_headers(buffer: [u8; 512]) -> HashMap<String, String> {
-    let request_str = String::from_utf8_lossy(&buffer[..]);
-    let splitted = request_str.split("\r\n");
-    println!("{:?}", splitted);
-
     let mut headers = HashMap::new();
-    headers.insert(
-        "status-line".to_string(),
-        "HTTP".to_string()
-    );
+    let request_str = String::from_utf8_lossy(&buffer[..]);
+    let mut splitted = request_str.split("\r\n");
+
+    if let Some(status) = splitted.next() {
+        let status_splitted: Vec<&str> = status.split(" ").collect();
+        headers.insert("Type".to_string(), status_splitted[0].to_string());
+        headers.insert("Route".to_string(), status_splitted[1].to_string());
+        headers.insert("Version".to_string(), status_splitted[2].to_string());
+    } 
+
+    for split in splitted {
+        let header_splitted: Vec<&str> = split.split(": ").collect();
+        if header_splitted.len() >= 2 {
+            headers.insert(
+                header_splitted[0].to_string(),
+                header_splitted[1].to_string(),
+            );
+        }
+    }
+
     headers
 } 
 
@@ -25,15 +37,22 @@ fn main() {
                 let mut buffer = [0; 512];
                 _stream.read(&mut buffer).unwrap();
 
-                println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-
                 let headers = extract_headers(buffer);
-                let response = if buffer.starts_with(b"GET / HTTP/1.1\r\n") { 
-                    "HTTP/1.1 200 OK\r\n\r\n"
-                } else {
-                    "HTTP/1.1 404 Not Found\r\n\r\n"
-                };
+                let mut response = String::new();
+                if let (Some(type_value), Some(route_value)) = (headers.get("Type"), headers.get("Route")) {
+                    if type_value == "GET" && route_value == "/" {
+                        response = "HTTP/1.1 200 OK\r\n\r\n".to_string();
+                    } else if type_value == "GET" && route_value.starts_with("/echo/") {
+                        let splitted: Vec<&str> = route_value.split("/").collect();
+                        let param = splitted[2];
+                        let length = param.len();
+                        response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", length, param);
+                    } else {
+                        response = "HTTP/1.1 404 Not Found\r\n\r\n".to_string();
+                    }
+                }
 
+                println!("{}", response);
                 _stream.write_all(response.as_bytes()).unwrap(); 
             }
             Err(e) => {
